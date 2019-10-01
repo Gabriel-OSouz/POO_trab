fef50046662a49d95847688da22bbd1d6c5dfa6d
Gabriel Oliveira <gabrieloliveirasouza620@gmail.com>
2019-10-01 07:38:47 -0300

    Commit teste

---
+++ Teste.java
@@ -0,0 +1,12 @@
+public class programajava
+{
+  public static void main(String[]args)
+  {
+  int i = 0;
+  while (i<4) {
+    System.out.println("Oi aqui:" + i);
+    i++;
+  }
+  System.out.println("Acabou por hoje.");
+}
+}
