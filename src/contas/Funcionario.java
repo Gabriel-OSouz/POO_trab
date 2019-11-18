3c1fe65a8cc3d3d781c64a3da739b11c8a9fb190
Leonardo Marinho de Melo Junior <leommjr09@gmail.com>
2019-11-18 21:41:40 -0200

    Primeiras classes

---
+++ src/contas/Funcionario.java
@@ -0,0 +1,18 @@
+package POO_trab.src.contas
+/**
+ * POO Trab UFU
+ * Class Funcionario
+ * @author Leonardo Marinho de Melo junior
+ * @author Vinicius Lopes Dias
+ * @author Gabriel Oliveira Souza
+ * @version 1.0
+ */
+public class Funcionario extends Pessoa implements Log
+{
+	protected String cargo;
+        protected double salario;
+	protected String ctps;
+}
+
+
+		
